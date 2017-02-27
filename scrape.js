9f0c6dd5d4a8f337c39681626d5dfcf97eb82e4a
Edward J. Schwartz <moo.git.z.edmcman@xoxy.net>
2017-02-26 21:06:51 -0500

    getting closer....

--- scrape.js
+++ scrape.js
@@ -1,4 +1,9 @@
-var casper = require("casper").create();
+var casper = require("casper").create({
+    viewportSize: {
+	width: 1920,
+	height: 1080
+    }
+});
 
 var email = casper.cli.get("email")
 var password = casper.cli.get("password")
@@ -18,6 +23,49 @@ casper.then(function() {
 
 casper.then(function() {});
 
+casper.on('resource.received', function(resource) {
+    if (resource.stage !== "end") {
+    	return;
+    }
+    casper.echo(resource.url);
+});
+
+function downloadImage(image) {
+    casper.echo("Downloading image " + image);
+    casper.then(function() {
+	this.click("div[id='" + image + "'] div.magnifying-icon");
+    });
+    casper.then(function() {
+	this.wait(10000, function() {
+	    this.capture("pic.png");
+	    // this.page.onFileDownload = function(status) { console.log('onfiledownload ' + status); };
+	    casper.echo("Clicking now...");
+	    this.click("li.download span");
+	});
+    });
+    casper.then(function() {
+	this.wait(10000, function() {
+	    casper.echo("hopefully it is here! " + this.getCurrentUrl());
+	    this.capture("download.png");
+	});
+    });
+};
+
+function processAlbum(caption) {
+    casper.echo("Processing " + caption);
+    casper.then(function() {
+	this.click("p[o_caption='" + caption + "']");
+	this.wait(10000, function() {
+	    this.capture("test.png");
+	    var pics = this.getElementsInfo("div.selectable-asset").map(function(x) {
+		return x.attributes.id;
+	    });
+	    require('utils').dump(pics);
+	    downloadImage(pics[0]);
+	});
+    });
+};
+
 casper.then(function() {
     this.wait(10000, function() {
 
@@ -26,12 +74,20 @@ casper.then(function() {
 	});
 	require('utils').dump(albums);
 
+	processAlbum(albums[0]);
 	// click the first one
-	// todo
-
+	
+	// this.click("p[o_caption=Winter]");
+	
     });
 });
 
+
+// casper.then(function() {
+//     this.wait(10000, function() {
+// 	this.capture("test.png");
+//     });
+// });
 
 casper.run(function() {
     this.echo('message sent').exit();
