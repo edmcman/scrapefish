c5e753f22059408b17850d77552b30471eade38f
Edward J Schwartz <edmcman@gmail.com>
2017-03-02 21:02:48 -0500

    grr

--- scrape.js
+++ scrape.js
@@ -2,6 +2,10 @@ var casper = require("casper").create({
     viewportSize: {
 	width: 1920,
 	height: 1080
+    },
+    onError: function(msg, backtrace) {
+        this.capture('error.png');
+        throw new ErrorFunc("fatal", "error", "filename", backtrace, msg);
     }
 });
 
@@ -13,41 +17,57 @@ casper.start('https://www.snapfish.com/photo-gift/loginto', function() {
         'EmailAddress':    email,
 	'Password': password
     }, true);
+    this.page.onFileDownload = function(status) {
+	console.log("onFileDownload " + status);
+	return "test.zip";
+    };
+    this.page.onNavigationRequested = function(a, b, c, d) {
+	console.log("navigation requested to " +a+b+c+d);
+    };
+    this.page.onResourceRequested = function(a, b) {
+	console.log('Request (#' + requestData.id + '): ' + JSON.stringify(requestData));
+    };
+    this.page.onPageCreated = function(newPage) {
+	console.log("A new child page was created, yay!");
+    };
 });
+
+//casper.thenOpen("https://github.com/edmcman/covize/archive/master.zip");
+
+casper.wait(10000);
 
 casper.then(function() {
-    this.wait(10000, function() {
-	this.click("a#myPhotosBtn");
-    });
+    this.echo("Logged in.  Clicking on my photos button now.");
+    this.click("a#myPhotosBtn");
 });
 
-casper.then(function() {});
-
-casper.on('resource.received', function(resource) {
-    if (resource.stage !== "end") {
-    	return;
-    }
-    casper.echo(resource.url);
-});
+//casper.then(function() {});
 
 function downloadImage(image) {
     casper.echo("Downloading image " + image);
     casper.then(function() {
 	this.click("div[id='" + image + "'] div.magnifying-icon");
     });
+    casper.wait(10000);
     casper.then(function() {
-	this.wait(10000, function() {
-	    this.capture("pic.png");
-	    // this.page.onFileDownload = function(status) { console.log('onfiledownload ' + status); };
-	    casper.echo("Clicking now...");
-	    this.click("li.download span");
-	});
+	this.capture("pic.png");
+	this.page.onFileDownload = function(status) { console.log('onfiledownload ' + status); };
+	casper.echo("Clicking now...");
+	this.click("li.download.detail-download");
+	casper.echo("Clicking now... a");
+	this.click("li.download.detail-download a");
+	casper.echo("Clicking now... b");
+	this.click("li.download.detail-download span");
+	casper.echo("Clicking now... c");
+	//this.click("li.download.detail-download bullshit");
+	//casper.echo("Clicking now... d");
+	//this.click("waaaaaat");
+	//casper.echo("Clicking now... e");
+	this.wait(10000);
     });
     casper.then(function() {
-	this.wait(10000, function() {
-	    casper.echo("hopefully it is here! " + this.getCurrentUrl());
-	    this.capture("download.png");
-	});
+	this.capture("debug.png");
+	this.echo("done...");
     });
 };
 
@@ -56,7 +76,7 @@ function processAlbum(caption) {
     casper.then(function() {
 	this.click("p[o_caption='" + caption + "']");
 	this.wait(10000, function() {
-	    this.capture("test.png");
+	    //this.capture("test.png");
 	    var pics = this.getElementsInfo("div.selectable-asset").map(function(x) {
 		return x.attributes.id;
 	    });
@@ -66,9 +86,11 @@ function processAlbum(caption) {
     });
 };
 
-casper.then(function() {
-    this.wait(10000, function() {
+casper.wait(10000);
 
+casper.then(function() {
+
+	casper.echo("Looking at my photos.");
 	var albums = this.getElementsInfo("p.storyCaption").map(function(x) {
 	    return x.attributes.o_caption;
 	});
@@ -80,7 +102,6 @@ casper.then(function() {
 	// this.click("p[o_caption=Winter]");
 	
     });
-});
 
 
 // casper.then(function() {
@@ -90,5 +111,6 @@ casper.then(function() {
 // });
 
 casper.run(function() {
-    this.echo('message sent').exit();
+    console.log("cool");
+    this.exit();
 });
