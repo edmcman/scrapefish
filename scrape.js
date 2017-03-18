767fee8b7b8699d30bf8406894101e82253bb9be
Edward J. Schwartz <moo.git.z.edmcman@xoxy.net>
2017-03-17 22:33:39 -0400

    More improvements.
    Currently stuck after the download because casperjs isn't realizing it's finished.

--- scrape.js
+++ scrape.js
@@ -1,18 +1,20 @@
 var casper = require("casper").create({
     verbose: true,
-    //logLevel: 'debug',
+    logLevel: 'debug',
+    silentErrors: false,
     viewportSize: {
 	width: 1920,
 	height: 1080
     },
     onError: function(msg, backtrace) {
-        this.capture('error.png');
 	console.log("ERROR: " + msg + backtrace);
+        this.capture('error.png');
         //throw new ErrorFunc("fatal", "error", "filename", backtrace, msg);
     }
 });
 
 var MAXALBUMS = 30;
+var MAXSCROLLS = 10;
 
 var fs = require('fs');
 var utils = require('utils');
@@ -24,6 +26,8 @@ if (!(email && password)) {
     throw "Define an email and password";
 }
 
+var downloadingurls = [];
+var numresources = 0;
 var downloaded = false;
 var dir = "";
  
@@ -39,8 +43,10 @@ casper.start('https://www.snapfish.com/photo-gift/loginto', function() {
 	console.log("File download error: " + err);
     };
     this.page.onFileDownload = function(url, responseData) {
+	downloadingurls.push(url);
 	utils.dump(responseData);
-	console.log('YEAH downloading');
+	console.log('YEAH downloading ' + url);
+	console.log(downloadingurls);
 	downloaded = true;
 	return dir + responseData.filename;
     };
@@ -50,29 +56,60 @@ casper.on("remote.message", function(msg) {
     this.echo("remote: " + msg);
 });
 
+casper.on("step.created", function(step) {
+    this.echo("STEP CREATED");
+});
+
+casper.on("step.start", function(step) {
+    this.echo("STEP START");
+});
+
+casper.on("step.error", function(step) {
+    this.echo("STEP ERROR");
+});
+
+// casper.on("resource.requested", function(resource, request) {
+//     numresources++;
+// });
+// casper.on("resource.timeout", function(resource) {
+//     numresources--;
+// });
+casper.on("resource.received", function(resource) {
+    if (resource.stage == "end" && downloadingurls.includes(resource.url)) {
+	this.echo("A file finished downloading! " + resource.url);
+	var i = downloadingurls.indexOf(resource.url);
+	this.echo("The index is " + i);
+	downloadingurls.splice(i, 1);
+	this.echo("downloadingurls.length = " + downloadingurls.length);
+	this.echo("downloadingurls = " + downloadingurls.toString());
+    }
+});
+
 casper.waitForSelector("a#myPhotosBtn", null, null, 10000).then(function() {
     this.echo("Logged in.  Clicking on my photos button now.");
     this.click("a#myPhotosBtn");
 });
 
-function downloadImage(image) {
-    casper.echo("Downloading image " + image);
-    casper.then(function() {
-	this.click("div[id='" + image + "'] div.magnifying-icon");
-    });
-    casper.waitForSelector("li.download.detail-download", null, null, 10000).then(function() {
-	//this.page.captureContent = [ /.*/ ]; // not sure
-	casper.echo("Clicking now...\n\n");
-	this.click("li.download.detail-download");
-    });
-    casper.waitFor(function() { return downloaded; },
-		   function() { this.echo("Image downloaded!"); },
-		   function() { this.echo("Image failed to download"); },
-		   10000);
-    casper.then(function() {
-	this.click("li.close.single");
-    });
-};
+// function downloadImage(image) {
+//     casper.echo("Downloading image " + image);
+//     casper.then(function() {
+// 	this.click("div[id='" + image + "'] div.magnifying-icon");
+//     });
+//     casper.waitForSelector("li.download.detail-download", null, null, 10000).then(function() {
+// 	//this.page.captureContent = [ /.*/ ]; // not sure
+// 	casper.echo("Clicking now...\n\n");
+// 	this.click("li.download.detail-download");
+//     });
+//     casper.waitFor(function() { return downloaded; },
+// 		   function() { this.echo("Image started to download!"); },
+// 		   function() { this.echo("Image failed to download"); },
+// 		   10000);
+//     // Let the download happen
+//     casper.wait(60000, function() {
+// 	this.echo("Done waiting for download. Hopefully it's done!");
+// 	this.click("li.close.single");
+//     });
+// };
 
 function processAlbum(month, year, caption) {
     casper.echo("Processing album " + caption);
@@ -81,41 +118,78 @@ function processAlbum(month, year, caption) {
 
     this.click('div[presentmonth="' + year + '-' + month + '"] p.storyCaption[o_caption="' + caption + '"]');
 
-    for (var i = 0; i < 10; i++) {
-	this.wait(10000, function() {
-	    var vispics = this.getElementsInfo("div.selectable-asset img")
-		.filter(function(x) { return !(x.attributes.src.includes("base64")); })
-		.map(function(x) { return x.attributes.id.replace("img_", ""); });
-	    this.echo("pics");
-	    utils.dump(vispics);
-	    // Get the id, remove img_, and look for the div with that id.
-	    // Click div.click-on-asset if id div does not contain selected class
-	    vispics.forEach(function(id) {
-		// utils.dump(this.getElementInfo("div[id=\"" + id + "\"]"));
-		if (!this.getElementInfo("div[id=\"" + id + "\"]").attributes.class.includes("selected")) {
-		    this.echo(id + " is unselected, going to click it");
-		    this.click("div[id=\"" + id + "\"] div.click-on-asset");
-		}
-	    }, this);
-     	    this.echo("Scrolling...");
-    	    this.evaluate(function() { $("div.scroll_sav_grid").scrollTop(10000); });
-	    this.evaluate(function() { console.log($("div.scroll_sav_grid").scrollTop()); });
-	    // XXX: Detect when we get to the bottom...
-	});
+    var atBottom = false;
 
-    }
-
-    // We've clicked all the pics, time to download.
-    this.wait(1000, function() { this.evaluate(function() { $("#bulkDownload").click(); }); });
-    this.wait(120000, function() {
-	downloaded = false;
-	// XXX: How do we know when we're done downloading?
-	// Deselect pictures
-	this.click("span.close_selected_assets");
-	this.click("a#globalHeaderMyPhotos");
-	loadMyPhotos();	
+    this.waitForSelector("div.scroll_sav_grid");
+    
+    this.repeat(MAXSCROLLS, function () {
+	this.waitFor(
+	    function check() {
+		return atBottom;
+	    },
+	    function thenf() {
+		this.echo("Not scrolling because we are at bottom");
+	    },
+	    function timeoutf() {
+		var vispics = this.getElementsInfo("div.selectable-asset img")
+		    .filter(function(x) { return !(x.attributes.src.includes("base64")); })
+		    .map(function(x) { return x.attributes.id.replace("img_", ""); });
+		this.echo("pics");
+		utils.dump(vispics);
+		// Get the id, remove img_, and look for the div with that id.
+		// Click div.click-on-asset if id div does not contain selected class
+		vispics.forEach(function(id) {
+		    // utils.dump(this.getElementInfo("div[id=\"" + id + "\"]"));
+		    if (!this.getElementInfo("div[id=\"" + id + "\"]").attributes.class.includes("selected")) {
+			this.echo(id + " is unselected, going to click it");
+			this.click("div[id=\"" + id + "\"] div.click-on-asset");
+		    }
+		}, this);
+     		this.echo("Scrolling...");
+    		atBottom = this.evaluate(function() {
+		    var orig = $("div.scroll_sav_grid").scrollTop();
+		    $("div.scroll_sav_grid").scrollTop(10000);
+		    var newy = $("div.scroll_sav_grid").scrollTop();
+		    console.log("old: " + orig + " new: " + newy);
+		    return orig === newy;
+		});
+	    }, 10000);
     });
 
+    // We better be at the bottom!
+    this.evaluateOrDie(function() {
+	var orig = $("div.scroll_sav_grid").scrollTop();
+	$("div.scroll_sav_grid").scrollTop(10000);
+	var newy = $("div.scroll_sav_grid").scrollTop();
+	console.log("old: " + orig + " new: " + newy);
+	return orig === newy;
+    });
+
+    // We've clicked all the pics, time to download.
+    this.wait(1000, function() {
+	downloaded = false;
+	this.evaluate(function() { $("#bulkDownload").click(); });
+    });
+
+    this.waitFor(
+	function() {
+	    return (downloaded && downloadingurls.length == 0);
+	},
+	function() {
+	    // Deselect pictures
+	    this.echo("Download finished!");
+	    this.click("span.close_selected_assets");
+	    this.click("a#globalHeaderMyPhotos");
+	    // loadMyPhotos();	
+	},
+	function() {
+	    this.die("Download failed");
+	},
+	120000);
+
+    this.then(function() {
+	this.echo("WTF!!!!");
+    });
 };
 
 function processMonth(month, year) {
