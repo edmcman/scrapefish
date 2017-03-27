0a30f65256ea0c6be5c11fb783cc084fb1b7de10
Edward J. Schwartz <moo.git.z.edmcman@xoxy.net>
2017-03-26 21:20:53 -0400

    Cleanups and comments and such

--- scrape.js
+++ scrape.js
@@ -1,22 +1,25 @@
-var casper = require("casper").create({
-    verbose: true,
-    logLevel: 'debug',
-    silentErrors: false,
-    viewportSize: {
-	width: 1280,
-	height: 700
-    },
-    onError: function(msg, backtrace) {
-	console.log("ERROR: " + msg + backtrace);
-        this.capture('error.png');
-        //throw new ErrorFunc("fatal", "error", "filename", backtrace, msg);
-    }
-});
-
 var MAXALBUMS = 30;
 var MAXSCROLLS = 500;
 var MAXPIX = 500;
 var LONGWAIT = 5*60*1000;
+
+var debug = true;
+
+var casper = require("casper").create({
+    verbose: true,
+    logLevel: 'debug',
+    silentErrors: false,
+    stepTimeout: LONGWAIT,
+    viewportSize: {
+	width: 1280,
+	height: 800
+    },
+    onError: function(msg, backtrace) {
+	console.log("ERROR: " + msg + backtrace);
+        this.capture('error.png');
+        throw new "fatal error";
+    }
+});
 
 var fs = require('fs');
 var utils = require('utils');
@@ -28,54 +31,59 @@ if (!(email && password)) {
     throw "Define an email and password";
 }
 
-var downloadsleft = 42;
+var downloadsleft = null;
 var downloadingurls = [];
-var dir = "";
+
+// Set by processAlbum
+var dir = null;
  
 casper.start('https://www.snapfish.com/photo-gift/loginto', function() {
     this.fill('form#form1', {
         'EmailAddress':    email,
 	'Password': password
     }, true);
-    this.page.onConsoleMessage = function(msg, lineNum, sourceId) {
-	console.log('CONSOLE: ' + msg + ' (from line #' + lineNum + ' in "' + sourceId + '")');
-    };
     this.page.onFileDownloadError = function(err) {
 	console.log("File download error: " + err);
     };
     this.page.onFileDownload = function(url, responseData) {
 	downloadingurls.push(url);
-	utils.dump(responseData);
-	console.log('YEAH downloading ' + url);
-	console.log(downloadingurls);
+	if (debug) {
+	    utils.dump(responseData);
+	    console.log('Adding ' + url + ' to urllist:');
+	    console.log(downloadingurls);
+	}
 	downloadsleft--;
 	if (downloadsleft == 0) {
-	    // Uh, this is a terrible hack for casperjs...
+	    casper.echo("Setting navigation requested = false");
 	    casper.navigationRequested = false;
+	    casper.echo("pendingWait: " + casper.pendingWait + " loadInProgress: " + casper.page.loadInProgress + " init: " + casper.page.browserInitializing)
 	}
 	return dir + responseData.filename;
     };
 });
 
-casper.on("remote.message", function(msg) {
-    this.echo("remote: " + msg);
-});
+if (debug) {
+    casper.on("remote.message", function(msg) {
+	this.echo("remote: " + msg);
+    });
 
-casper.on("step.created", function(step) {
-    this.echo("STEP CREATED");
-});
+    casper.on("step.created", function(step) {
+	this.echo("STEP CREATED");
+    });
 
-casper.on("step.start", function(step) {
-    this.echo("STEP START");
-});
+    casper.on("step.start", function(step) {
+	this.echo("STEP START");
+    });
 
-casper.on("step.error", function(step) {
-    this.echo("STEP ERROR");
-});
+    casper.on("step.error", function(step) {
+	this.echo("STEP ERROR");
+    });
+}
 
 var imageurl = null;
 var imagesuccess = false;
 var numresourcespending = 0;
+
 casper.on("resource.requested", function(resource, request) {
     numresourcespending++;
 });
@@ -83,78 +91,84 @@ casper.on("resource.timeout", function(resource) {
     //numresourcespending--;
     this.echo("resource timeout! " + resource.url);
 });
+
+// Used to detect when there are no resources pending.
 casper.on("resource.received", function(resource) {
     if (resource.stage == "end") {
 	numresourcespending--;
-	this.echo("numresourcespending = " + numresourcespending);	
+	if (debug) {
+	    this.echo("numresourcespending = " + numresourcespending);
+	}
     }
 });
+
+// Used to detect when the detailed image view has its image url
+// loaded, and whether it downloaded successfully or not.
 casper.on("resource.received", function(resource) {
-    //this.echo("imageurl = " + imageurl + " resource.url = " + resource.url);
-    //utils.dump(resource);
+    if (debug) {
+	this.echo("imageurl = " + imageurl + " resource.url = " + resource.url);
+	//utils.dump(resource);
+    }
     if (resource.stage == "end" && resource.url == imageurl) {
 	// They are redirecting us.  Record the new imageurl
 	if (resource.status == 302) {
 	    imageurl = resource.redirectURL;
 	}
 	imagesuccess = resource.status == 200;
-	// this.echo("imagesuccess = " + imagesuccess);
+	if (debug) {
+	    this.echo("imagesuccess = " + imagesuccess);
+	}
     }
 });
+
+// Keep track of pending downloads.
 casper.on("resource.received", function(resource) {
     if (resource.stage == "end" && downloadingurls.includes(resource.url)) {
-	this.echo("A file finished downloading! " + resource.url);
+	// Remove url from list
 	var i = downloadingurls.indexOf(resource.url);
-	this.echo("The index is " + i);
 	downloadingurls.splice(i, 1);
-	this.echo("downloadingurls.length = " + downloadingurls.length);
-	this.echo("downloadingurls = " + downloadingurls.toString());
+
+	if (debug) {
+	    this.echo("A file finished downloading! " + resource.url);
+	    this.echo("downloadingurls.length = " + downloadingurls.length);
+	    this.echo("downloadingurls = " + downloadingurls.toString());
+	}
+
+	if (downloadingurls.length == 0) {
+	    casper.echo("Setting navigation requested = false");
+	    casper.navigationRequested = false;
+	    casper.echo("pendingWait: " + casper.pendingWait + " loadInProgress: " + casper.page.loadInProgress + " init: " + casper.page.browserInitializing);
+	}
     }
 });
 
-
-// function downloadImage(image) {
-//     casper.echo("Downloading image " + image);
-//     casper.then(function() {
-// 	this.click("div[id='" + image + "'] div.magnifying-icon");
-//     });
-//     casper.waitForSelector("li.download.detail-download", null, null, 10000).then(function() {
-// 	//this.page.captureContent = [ /.*/ ]; // not sure
-// 	casper.echo("Clicking now...\n\n");
-// 	this.click("li.download.detail-download");
-//     });
-//     casper.waitFor(function() { return downloaded; },
-// 		   function() { this.echo("Image started to download!"); },
-// 		   function() { this.echo("Image failed to download"); },
-// 		   10000);
-//     // Let the download happen
-//     casper.wait(60000, function() {
-// 	this.echo("Done waiting for download. Hopefully it's done!");
-// 	this.click("li.close.single");
-//     });
-// };
 
 function scrollToBottomOfSelector(selector, wait_ms=30000, max_scrolls=MAXSCROLLS, ensurebottom=true, waitfunc=function() {}) {
     var atBottom = false;
-    casper.then(function() {
-	this.echo("I am executing scrollToBottom here!");
-    });
+    if (debug) {
+	casper.then(function() {
+	    this.echo("I am executing scrollToBottom here!");
+	});
+    }
     casper.repeat(max_scrolls, function() {
 	this.waitFor(
 	    function check() {
 		return atBottom;
 	    },
 	    function thenf() {
-		this.echo("Not scrolling because we are at bottom");
+		if (debug) {
+		    this.echo("Not scrolling because we are at bottom");
+		}
 	    },
 	    function timeoutf() {
-		this.echo("Scrolling");
+		if (debug) {
+		    this.echo("Scrolling");
+		}
 		atBottom = this.evaluate(function(selector) {
 		    var orig = $(selector).scrollTop();
 		    $(selector).scrollTop(100000);
-		    //waitfunc.call(this);
 		    var newy = $(selector).scrollTop();
-		    console.log("scrolling old: " + orig + " new: " + newy);
+		    //console.log("scrolling old: " + orig + " new: " + newy);
 		    return orig === newy;
 		}, selector);
 	    },
@@ -181,14 +195,16 @@ function processAlbum(month, year, caption) {
     var atBottom = false;
     var atEnd = false;
 
-    this.echo("magnifying icon is here...");
     this.waitForSelector("div.magnifying-icon");
     this.thenClick('div.magnifying-icon');
     this.waitForSelector("img.detailedViewImg");
 
     for (var i = 0; i < MAXPIX; i++) {
 
-	this.then(function() { this.echo("Waiting for new url..."); });
+	if (debug) {
+	    this.then(function() { this.echo("Waiting for new url..."); });
+	}
+	// Wait for the big image preview to load (or fail to load)
 	this.waitFor(
 	    function check() {
 		if (atEnd) { return true; }
@@ -197,19 +213,24 @@ function processAlbum(month, year, caption) {
 		imageurl = newurl;
 		return ret;
 	    }, null, null, LONGWAIT);
-	this.then(function() { this.echo("Waiting for resources"); });
+	if (debug) {
+	    this.then(function() { this.echo("Waiting for resources"); });
+	}
 	this.waitFor(
 	    function check() {
 		return numresourcespending == 0 || atEnd;
 	    }, null, null, LONGWAIT);
 	this.then(function() {
 	    if (atEnd) return;
-	    this.echo("Image there? " + imagesuccess);
+	    if (debug) {
+		this.echo("Image there? " + imagesuccess);
+	    }
 	    if (imagesuccess) {
-		//this.click("figure.ib-checkmark");
+		// Click the download button
 		downloadsleft = 1;
 		this.click("li.download.detail-download");
 	    } else {
+		// Download the thumbnail
 		var thumburl = this.evaluate(function() {
 		    var t = $("li.bottom-thumbnail.selected").css("background-image");
 		    // Get rid of the url() wrapper.
@@ -218,28 +239,33 @@ function processAlbum(month, year, caption) {
 		var thumbfilename = dir + this.evaluate(function() {
 		    return $("li.bottom-thumbnail.selected").attr("id") + ".jpg";
 		});
-		this.echo("Thumbnail url = " + thumburl + " filename = " + thumbfilename);
+		if (debug) {
+		    this.echo("Thumbnail url = " + thumburl + " filename = " + thumbfilename);
+		}
 		this.download(thumburl, thumbfilename);
-		this.echo("Download done!");
 		downloadsleft = 0;
 	    }
-	    imageurl = null;
 	    imagesuccess = false;
 	});
-	// Wait for the download to happen
+	// Wait for the download to finish
 	this.waitFor(function check() {
 	    return (downloadsleft == 0 && numresourcespending == 0) || atEnd;
 	}, null, null, LONGWAIT);
 
 	// Are we at the end of the album?
 	this.then(function() {
+	    imageurl = null;
 	    if (this.evaluate(function() {
 		return $("figure.right-nav").is(":visible");
 	    })) {
-		this.echo("Right arrow is visible...");
+		if (debug) {
+		    this.echo("Right arrow is visible...");
+		}
 		this.click("figure.right-nav");
 	    } else {
-		this.echo("At the end of the album!");
+		if (debug) {
+		    this.echo("At the end of the album!");
+		}
 		atEnd = true;
 	    }
 	});
@@ -249,20 +275,6 @@ function processAlbum(month, year, caption) {
     this.thenClick("li.close.single");
     this.thenClick("a#globalHeaderMyPhotos");
     loadMyPhotos();
-
-};
-
-function processMonth(month, year) {
-    this.echo("Downloading month " + month + " of year " + year);
-
-    var albums;
-    try {
-	albums = this.getElementsInfo('div[presentmonth="' + year + '-' + month + '"] p.storyCaption');
-    } catch(err) {
-	albums = [];
-    }
-
-    utils.dump(albums);
 
 };
 
@@ -332,7 +344,6 @@ casper.repeat(MAXALBUMS, function() {
 	processAlbum.call(this, albums[i][0], albums[i][1], albums[i][2]);
     }
 });
-
 
 casper.run(function() {
     console.log("Exiting");
